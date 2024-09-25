74102793275274aa5b5d744b474298df7beaf4b7
harmlessprince <realolamilekan@gmail.com>
2024-09-25 09:04:02 +0100

    added router

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import {createMemoryHistory, createRouter} from 'vue-router'
+
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
+import NotFound from "@/pages/NotFound.vue";
+
+const routes = [
+    {path: '/', component: Home, name: 'home'},
+    {path: '/about', component: About, name: 'about'},
+    {
+        path: "/:pathMatch(.*)*",
+        name: "NotFound",
+        component: NotFound,
+    },
+]
+
+const router = createRouter({
+    history: createMemoryHistory(import.meta.env.BASE_URL),
+    linkActiveClass: "active-link",
+    linkExactActiveClass: "exact-active-link",
+    routes,
+})
