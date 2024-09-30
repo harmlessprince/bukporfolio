cb765a432d7d696e845a4358ba913df042a7b45b
chiaka <Osuji.c@carrotsuite.xyz>
2024-09-30 21:02:16 +0100

    worked on footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -8,7 +8,7 @@ import Instagram from '@/assets/instagram.svg';
 </script>
 
 <template>
-  <footer class="sticky bottom-0 mt-[5rem] text-center w-full bg-[#0f0f13] py-[1rem] text-[#fff]">
+  <footer class="mt-[5rem] text-center w-full bg-[#0f0f13] py-[1rem] text-[#fff]">
     <img :src="UkLogo" class="w-[6.4rem] h-[6.4rem] mx-auto" alt="footer logo" />
 
     <div class="text-basic font-xsm flex flex-row w-full justify-center mt-sm">
