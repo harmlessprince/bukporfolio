f5fc7293104cb2d585a6eaaf51afc809c09369f2
Emmanuel Ayodele <eayodeleof18@gmail.com>
2024-09-30 21:50:55 +0100

    fixed testimonials

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -10,20 +10,27 @@ const store = useFeaturesStore()
 onBeforeMount(() => {
   store.getFeatures();
 });
-
 </script>
 
 <template>
   <Container>
     <section class="flex items-center py-top bg-white overflow-hidden">
-      <div class="font-header text-xlg text-[#0F0F13] mr-[2rem] no-wrap w-[9rem]">Featured on:</div>
+      <div
+        class="font-header text-xlg text-[#0F0F13] mr-[2rem] whitespace-nowrap"
+      >
+        Featured on:
+      </div>
       <div class="flex flex-row justify-between items-center w-full">
-        <img :src="item.image ?? Brand1" class="w-[17.1rem] h-[6.5rem] mr-[2rem] grayscale" :alt="item.name"  v-for="(item, index) in store.features" :key="index"/>
+        <img
+          :src="item.image ?? Brand1"
+          class="w-[17.1rem] h-[6.5rem] mr-[2rem] grayscale"
+          :alt="item.name"
+          v-for="(item, index) in store.features"
+          :key="index"
+        />
       </div>
     </section>
   </Container>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Testimonies.vue
+++ src/components/Testimonies.vue
@@ -49,49 +49,83 @@ function previousQuote() {
   currentQuote.value = store.testimonies[currentIndex.value];
   currentDirection.value = 'backward';
 }
-
 </script>
 
 <template>
-  <section class="">
+  <section class="grow-[2] shrink-0 basis-0">
     <Container>
-      <Subheading title="Testimony"/>
-      <Heading title="What People are Saying"/>
+      <h3
+        class="font-lg text-basic text-primary font-primary leading-[2rem] mb-[0.7rem]"
+      >
+        Testimony
+      </h3>
+      <h2
+        class="font-header text-header text-secondary leading-[4.2rem] mb-[2.6rem]"
+      >
+        What People are Saying
+      </h2>
       <!-- this part will be dynamic, we will work on it -->
-      <div class="bg-[#F9F7EA] rounded-[10px] px-[1rem] py-[2rem] mt-[1rem]" v-if="currentQuote">
-        <i class="font-sm text-secondary text-xlg">
+      <div
+        class="bg-[#F9F7EA] rounded-[10px] px-[2.2rem] py-[3.7rem] h-[25.4rem] flex flex-col gap-[2rem] justify-between"
+        v-if="currentQuote"
+      >
+        <q
+          class="font-sm text-secondary text-xlg italic mb-[1.7rem] leading-[3rem] overflow-clip"
+        >
           {{ currentQuote.quote }}
-        </i>
+        </q>
 
-        <div class="flex flex-row justify-between items-end mt-[2rem]">
+        <div class="flex flex-row justify-between items-end">
           <div class="flex flex-row">
-            <img :src="currentQuote.image" class="w-[55px] h-[59px] mr-[1rem] rounded-full" alt="bright"/>
-            <span class="font-header text-xlg text-secondary">{{ currentQuote.name }}<br/>
-                <span
-                    class="font-sm text-[#838080] text-xsm">{{
+            <img
+              :src="currentQuote.image"
+              class="w-[55px] h-[59px] mr-[1rem] rounded-full"
+              alt="bright"
+            />
+            <span class="font-header text-xlg text-secondary"
+              >{{ currentQuote.name }}<br />
+              <span class="font-sm text-[#838080] text-xsm"
+                >{{
                     currentQuote.position ?? ''
-                  }} of {{ currentQuote.company ?? '' }}</span>
-                </span>
+                }}
+                of {{ currentQuote.company ?? '' }}</span
+              >
+            </span>
           </div>
-          <img :src="CommentIcon1" class="w-[24px] h-[18px] mr-[1rem]" alt="comment"/>
+          <img
+            :src="CommentIcon1"
+            class="w-[24px] h-[18px] mr-[1rem]"
+            alt="comment"
+          />
         </div>
       </div>
       <div class="mt-[2rem] flex flex-row justify-between items-center">
         <div>
           <!-- using google material icon, cdn link is in the index page -->
           <span
-              class="material-icons w-[26px] h-[27px] mr-[2rem]"
-              :class="currentDirection === 'backward' ? 'text-[#151515]' : 'text-[#B5B5B5]'"
-              @click="previousQuote"
-          >arrow_back</span>
-          <span class="material-icons w-[26px] h-[27px]" @click="nextQuote" :class="currentDirection === 'forward' ? 'text-[#151515]' : 'text-[#B5B5B5]'">arrow_forward</span>
+            class="material-icons w-[26px] h-[27px] mr-[2rem]"
+            :class="currentDirection === 'backward' ? 'text-[#151515]' : 'text-[#B5B5B5]'"
+            @click="previousQuote"
+            >arrow_back</span
+          >
+          <span
+            class="material-icons w-[26px] h-[27px]"
+            @click="nextQuote"
+            :class="currentDirection === 'forward' ? 'text-[#151515]' : 'text-[#B5B5B5]'"
+            >arrow_forward</span
+          >
         </div>
 
         <div class="flex flex-row">
-          <img :src="item.image" :alt="item.name" v-for="(item, index) in store.testimonies" :key="index"
-               class="w-[4.2rem] h-[4.5rem] rounded-full space-x-1"
-               :class="currentQuote?.id === item.id ? 'opacity-40': ''"
-               @click="onQuoteChange(index)"/>
+          <img
+            :src="item.image"
+            :alt="item.name"
+            v-for="(item, index) in store.testimonies"
+            :key="index"
+            class="w-[4.2rem] h-[4.5rem] rounded-full space-x-1"
+            :class="currentQuote?.id === item.id ? 'opacity-40': ''"
+            @click="onQuoteChange(index)"
+          />
         </div>
       </div>
       <!-- dynamic part of testimony ends here -->
@@ -99,6 +133,4 @@ function previousQuote() {
   </section>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
