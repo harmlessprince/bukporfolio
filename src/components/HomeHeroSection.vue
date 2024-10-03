92994e2a0450e3b3ce1a1cbfe2b2d83fe9349143
harmlessprince <realolamilekan@gmail.com>
2024-10-03 11:08:22 +0100

    fixed home screen animation

--- src/components/HomeHeroSection.vue
+++ src/components/HomeHeroSection.vue
@@ -3,30 +3,51 @@
 import HomeBanner from "@/assets/home_banner_background.png";
 import HomeBright1 from "@/assets/home_banner_bright.png";
 import Container from "@/components/Container.vue";
+import {ref} from "vue";
+
+const quotes = ref([
+  {
+    title: "CHANGE YOUR LIFE",
+    description: "You are not disadvantaged as you think, you are only disadvantaged by your thinking",
+  },
+  {
+    title: "ELEVATE YOUR LEADERSHIP",
+    description: "Your growth is your factor of inspiration. As you grow into the best version of yourself, you give others the permission to do the same."
+  },
+  {
+    title: "CREATE YOUR OWN POSSIBILITIES",
+    description: "You can create wealth without capital if you know how to leverage capital beyond money."
+  },
+  {
+    title: "CHANGE YOUR LIFE",
+    description: "You are not disadvantaged as you think, you are only disadvantaged by your thinking",
+  },
+])
 </script>
 
 <template>
   <section class="bg-secondary mt-[9.3rem] max-sm:py-[3rem] h-[52.2rem] max-sm:h-full relative">
     <Container
-      class="w-[98rem] h-full overflow-hidden text-basic flex max-sm:flex-col justify-center items-center flex-nowrap gap-4"
-      :style="{ 'background-image': 'url(' + HomeBanner + ')' }"
+        class="w-[98rem] h-full overflow-hidden text-basic flex max-sm:flex-col justify-center items-center flex-nowrap gap-4"
+        :style="{ 'background-image': 'url(' + HomeBanner + ')' }"
     >
       <div class="grow-[2] flex-shrink-0 basis-0">
-        <div>
-          <h1 class="text-white font-bold font-title text-[4.8rem] mb-[2.1rem]">
-            CHANGE YOUR THINKING
-          </h1>
-          <h2
-            class="text-primary font-bold font-title text-[3.5rem] mb-[2.1rem]"
-          >
-            TO CHANGE YOUR LIFE
-          </h2>
-          <h3 class="text-[#949494] text-[2rem] mb-[4.4rem] font-normal">
-            You are not disadvantaged as you think, you are only disadvantaged
-            by your thinking
-          </h3>
+        <div class="text-white font-bold font-title text-[4.8rem] mb-[2.1rem]">
+          CHANGE YOUR THINKING TO
+          <span
+              class="text-primary font-bold font-title text-[3.5rem] mb-[2.1rem] block h-[15rem] overflow-hidden">
+            <ul class="block text-left [&_li]:block animate-text-slide-3">
+
+              <li class="mb-[4.4rem]" v-for="(item, index) in quotes" :key="index" :aria-hidden="index === quotes.length - 1 ? 'true' : 'false'">
+                 <span>{{item.title}}</span>
+                 <p class="text-[#949494] text-[2rem] font-normal mt-2">
+                   {{item.description}}
+                  </p>
+              </li>
+            </ul>
+          </span>
           <button
-            class="bg-primary rounded-[6px] border border-primary text-btnText text-xsm py-[1.75rem] px-[6rem] font-sm"
+              class="bg-primary rounded-[6px] border border-primary text-btnText text-xsm py-[1.75rem] px-[6rem] font-sm"
           >
             Book Bright UK
           </button>
@@ -34,13 +55,15 @@ import Container from "@/components/Container.vue";
       </div>
       <div class="h-full w-full flex-grow flex-shrink-0 basis-0 max-sm:hidden">
         <img
-          :src="HomeBright1"
-          class="w-full h-full object-cover object-center"
-          alt="homebanner"
+            :src="HomeBright1"
+            class="w-full h-full object-cover object-center"
+            alt="homebanner"
         />
       </div>
     </Container>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,84 +1,117 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
-  plugins: [require("flowbite/plugin")],
-  theme: {
-    extend: {
-      keyframes: {
-        slideInFromLeft: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
+    plugins: [require("flowbite/plugin")],
+    theme: {
+        extend: {
+            keyframes: {
+                slideInFromLeft: {
+                    '0%': {transform: 'translateX(-100%)', opacity: '0'},
+                    '100%': {transform: 'translateX(0)', opacity: '1'},
+                },
+                slideInFromRight: {
+                    '0%': {transform: 'translateX(100%)', opacity: '0'},
+                    '100%': {transform: 'translateX(0)', opacity: '1'},
+                },
+                'text-slide-3': {
+                    '0%, 26.66%': {
+                        transform: 'translateY(0%)',
+                    },
+                    '33.33%, 60%': {
+                        transform: 'translateY(-25%)',
+                    },
+                    '66.66%, 93.33%': {
+                        transform: 'translateY(-50%)',
+                    },
+                    '100%': {
+                        transform: 'translateY(-75%)',
+                    },
+                },
+                'text-slide-4': {
+                    '0%, 20%': {
+                        transform: 'translateY(0%)',
+                    },
+                    '25%, 45%': {
+                        transform: 'translateY(-20%)',
+                    },
+                    '50%, 70%': {
+                        transform: 'translateY(-40%)',
+                    },
+                    '75%, 95%': {
+                        transform: 'translateY(-60%)',
+                    },
+                    '100%': {
+                        transform: 'translateY(-80%)',
+                    },
+                },
+            },
+            animation: {
+                slideInLeft: 'slideInFromLeft 0.5s ease-out forwards',
+                slideInRight: 'slideInFromRight 0.5s ease-out forwards',
+                'text-slide-3': 'text-slide-3 7.5s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+                'text-slide-4': 'text-slide-4 10s cubic-bezier(0.83, 0, 0.17, 1) infinite',
+            },
+            fontSize: {
+                xsm: "1.4rem", //14px
+                sm: "1.5rem", //15px
+                lg: "1.8rem", //18px
+                xlg: "2rem", //20px
+                basic: "1.6rem", //16px
+                basicText: "1.6rem",
+                forty: "4.0rem", //40px
+                hero: "5.2rem", //52px
+                header: "3.6rem" //36px
+            },
+            padding: {
+                msides: "3rem", //for mobile
+                tsides: "8rem", //tablet
+                sides: "15rem",
+                top: "3rem",
+            },
+            margin: {
+                top: "3rem",
+                sm: "2rem",
+                regular: "5rem",
+                xsm: "1rem"
+            },
+
+            fontWeight: {
+                xsm: "400",
+                xsmall: "400",
+                sm: "500",
+                lg: "600",
+                xlg: "900",
+                header: "700",
+                hero: "800",
+            },
+
+            colors: {
+                primary: "#C7AE2E",
+                primaryColor: "#C7AE2E",
+                secondary: "#000000", //black
+                secondaryColor: "#000000",
+                basicColor: "#FFFFFF", //white
+                navLinkColor: "#F3F3F3",
+                bannerNavColor: "#C3C3C3",
+                btnText: "#242424",
+                btnTextColor: "#242424",
+                regular: "#757575",
+                regularColor: "#757575",
+                bg1: "#DADADA",
+                bg1Color: "#DADADA",
+                mainBorder: "#EEF0F3",
+            },
+
+            screens: {
+                small: "400px",
+                medium: "768px",
+                large: "900px",
+                xlarge: "1900px",
+            },
+            fontFamily: {
+                primary: ["Inter"],
+                title: ["Roboto", "sans-serif"],
+            },
         },
-        slideInFromRight: {
-          '0%': { transform: 'translateX(100%)', opacity: '0' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        slideInLeft: 'slideInFromLeft 0.5s ease-out forwards',
-        slideInRight: 'slideInFromRight 0.5s ease-out forwards',
-      },
-      fontSize:{
-          xsm: "1.4rem", //14px
-          sm: "1.5rem", //15px
-          lg: "1.8rem", //18px
-          xlg: "2rem", //20px
-          basic: "1.6rem", //16px
-          basicText: "1.6rem",
-          forty: "4.0rem", //40px
-          hero: "5.2rem", //52px
-          header: "3.6rem" //36px
-      },
-      padding: {
-        msides: "3rem", //for mobile
-        tsides: "8rem", //tablet
-        sides: "15rem",
-        top: "3rem",
-      },
-      margin: {
-        top: "3rem",
-        sm: "2rem",
-        regular: "5rem",
-        xsm: "1rem"
-      },
-
-      fontWeight: {
-        xsm: "400",
-        xsmall: "400",
-        sm: "500",
-        lg: "600",
-        xlg: "900",
-        header: "700",
-        hero: "800",
-      },
-
-      colors: {
-        primary: "#C7AE2E",
-        primaryColor: "#C7AE2E",
-        secondary: "#000000", //black
-        secondaryColor: "#000000",
-        basicColor: "#FFFFFF", //white
-        navLinkColor: "#F3F3F3",
-        bannerNavColor: "#C3C3C3",
-        btnText: "#242424",
-        btnTextColor: "#242424",
-        regular: "#757575",
-        regularColor: "#757575",
-        bg1: "#DADADA",
-        bg1Color: "#DADADA",
-        mainBorder: "#EEF0F3",
-      },
-
-      screens: {
-        small: "400px",
-        medium: "768px",
-        large: "900px",
-        xlarge: "1900px",
-      },
-      fontFamily: {
-        primary: ["Inter"],
-        title: ["Roboto", "sans-serif"],
-      },
     },
-  },
 };
