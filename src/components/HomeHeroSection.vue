9c1d7f903fe83988f90497287a30f214af025840
chiaka <Osuji.c@carrotsuite.xyz>
2024-10-07 23:54:30 +0100

    worked on the home banner animation. it is now responsive

--- src/components/HomeHeroSection.vue
+++ src/components/HomeHeroSection.vue
@@ -3,6 +3,10 @@ import HomeBanner from "@/assets/home_banner_background.png";
 import HomeBright1 from "@/assets/home_banner_bright.png";
 import Container from "@/components/Container.vue";
 import {ref, onMounted, onBeforeUnmount} from "vue";
+import { gsap } from "gsap";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 const quotes = ref([
   {
@@ -16,12 +20,40 @@ const quotes = ref([
   {
     title: "CREATE YOUR OWN POSSIBILITIES",
     description: "You can create wealth without capital if you know how to leverage capital beyond money."
-  },
-  {
-    title: "CHANGE YOUR LIFE",
-    description: "You are not disadvantaged as you think, you are only disadvantaged by your thinking",
-  },
+  }
 ])
+
+const quotesCount = ref(1)
+let timeoutId;
+
+const incrementCount = () => {
+  quotesCount.value += 1;
+}
+
+const resetQuotesCount = () => {
+  quotesCount.value = 0;
+}
+
+    onMounted(() => {
+      timeoutId = setInterval(() => {
+        let zeroBasedQuotesLength = quotes.value.length - 1;
+        zeroBasedQuotesLength > quotesCount.value ?  incrementCount() : resetQuotesCount()
+        gsap.fromTo(".subTextBox",
+        { y: 150 }, 
+        { y: 0, 
+          duration: 1, 
+          scrollTrigger: {
+          trigger: ".subTextBox",
+        },
+        });    
+      }, 5000);
+    });
+
+    onBeforeUnmount(() => {
+      // Cleanup if component is destroyed
+      clearTimeout(timeoutId);
+    });
+
 
 </script>
 
@@ -36,15 +68,21 @@ const quotes = ref([
         <div class="text-white font-bold font-title text-[4.8rem] max-sm:text-[3.6rem] max-sm:leading-[44px] mb-[2.1rem] max-sm:mb-[1rem]">
           CHANGE YOUR THINKING TO
           <span
-              class="text-primary font-bold font-title text-[3.5rem] mb-[2.1rem] block h-[15rem] max-sm:h-[26.5rem] overflow-hidden">
-            <ul class="block text-left [&_li]:block animate-text-slide-3">
-
-              <li class="mb-[4.4rem] max-sm:mb-0 mt-[1rem] max-sm:py-[2rem]" v-for="(item, index) in quotes" :key="index" :aria-hidden="index === quotes.length - 1 ? 'true' : 'false'">
+              class="text-primary font-bold font-title text-[3.5rem] mb-[2.1rem] block h-[15rem] max-small:h-[26.5rem] small:max-medium:h-[22rem] medium:max-large:h-[18.5rem] overflow-hidden">
+            <!-- <ul class="block text-left [&_li]:block animate-text-slide-3"> -->
+              <ul class="block text-left [&_li]:block subTextBox">
+              <!-- <li class="max-h-[45rem] mb-[4.4rem] max-sm:mb-0 mt-[1rem] max-sm:mt-0 max-sm:py-[2rem]" v-for="(item, index) in quotes" :key="index" :aria-hidden="index === quotes.length - 1 ? 'true' : 'false'">
                  <span>{{item.title}}</span>
                  <p class="text-[#949494] text-[2rem] font-normal mt-2 max-sm:text-[1.rem] mb-[2.1rem] max-sm:mb-0">
                    {{item.description}}
                   </p>
-              </li>
+              </li> -->
+              <li class="mb-[4.4rem] max-sm:mb-0 mt-[1rem] max-sm:mt-0 max-sm:py-[2rem]">
+                <span>{{quotes[quotesCount].title}}</span>
+                <p class="text-[#949494] text-[2rem] font-normal mt-2 mb-[2.1rem] max-medium:leading-[32px] max-sm:mb-0">
+                  {{quotes[quotesCount].description}}
+                 </p>
+             </li>
             </ul>
           </span>
           <RouterLink
