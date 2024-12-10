30d61d32051604bc7e7e122213ee899087cda628
harmlessprince <realolamilekan@gmail.com>
2024-12-10 15:07:34 +0100

    completed quotes

--- src/components/GroupedQuotes.vue
+++ src/components/GroupedQuotes.vue
@@ -10,7 +10,7 @@ const slideTo = (nextSlide) => {
   console.log(nextSlide);
   console.log("Before: ", currentSlide.value);
   currentSlide.value = nextSlide
-  console.log("After: ",currentSlide.value);
+  console.log("After: ", currentSlide.value);
 };
 
 const quotesConfig = {
@@ -25,80 +25,90 @@ const quotesConfig = {
 
   <div
       v-if="index%2 === 0"
-      class="inspirationContainer cursor-pointer relative w-full min-h-[28.4rem] rounded-[40px] p-[1.2rem] text-left mb-[2rem] max-small:mb-0 flex items-center justify-center"
+      class="inspirationContainer cursor-pointer w-full h-[35rem] rounded-[40px] p-[1.2rem] text-left mb-[2rem] max-small:mb-0"
       :style="{'background-color': `#e8d29b`}"
 
   >
-    <div class="text-center text-[#000] iconDiv">
+    <div class="quoteIcon ">
+      <img :src="Quotation" class="w-[2.5rem] h-[2.5rem] justify-end " alt="quote icone"/>
+    </div>
+    <!--    Front start-->
+    <div class="text-center text-[#000] iconDiv h-full flex flex-col items-center justify-center">
       <img :src="props.icon" class="w-[11.3rem] z-50 h-[11.7rem] mx-auto" alt="about speaker bright uk"/>
       <header class="text-[2.2rem] leading-[24px] font-[700] mt-[1.5rem]">{{ props.title }}</header>
     </div>
-    <div
-        class="qoutation text-[#000] font-[400] text-[1.4rem] px-[1.6rem] leading-[20px] absolute top-0 left-0 pt-[4rem] pb-[0.5rem]">
-      <img :src="Quotation" class="absolute top-[1.5rem] right-[1.5rem] w-[2.5rem] z-50 h-[2.5rem]"
-           alt="about speaker bright uk"/>
-      <div class="flex flex-row gap-x-[0.5rem] mb-[1rem] items-center">
-        <img :src="props.icon" class="w-[2.5rem] z-50 h-[2.5rem]" alt="about speaker bright uk"/>
-        <header class="text-[1.8rem] leading-[24px] font-[700]">{{ props.title }}</header>
-      </div>
-      <div class="flex flex-col justify-between">
+    <!--    Front end-->
+    <!--    Back start-->
+    <div class="qoutation">
+      <div>
+        <div class="flex flex-row gap-x-[0.5rem] mb-[1rem] items-center">
+          <img :src="props.icon" class="w-[2.5rem] z-50 h-[2.5rem]" alt="about speaker bright uk"/>
+          <header class="text-[1.8rem] leading-[24px] font-[700]">{{ props.title }}</header>
+        </div>
         <div class="flex mb-2">
-          <div  v-for="(slide, index) in props.quotes" :key="index" v-show="index === currentSlide">
-            <p class="text-left">{{ slide }}</p>
+          <div v-for="(slide, index) in props.quotes" :key="index" v-show="index === currentSlide">
+            <p class="text-left">{{ slide.substring(0, 350) }}</p>
           </div>
         </div>
-        <div class="flex space-x-[0.5rem] justify-end items-center">
-          <button
-              v-for="(_, index) in props.quotes"
-              :key="`${index}`+'button'"
-              @click="slideTo(index)"
-              :class="index === currentSlide ? 'bg-black text-white' : 'bg-gray-50 text-black'"
-              class="round_pagination_button "
-          >
-          </button>
-          <p>{{currentSlide + 1}}/{{props.quotes.length}}</p>
-        </div>
       </div>
+
+      <div class="flex space-x-[0.5rem] justify-end items-center mt-auto">
+        <button
+            v-for="(_, index) in props.quotes"
+            :key="`${index}`+'button'"
+            @click="slideTo(index)"
+            :class="index === currentSlide ? 'bg-black text-white' : 'bg-gray-50 text-black'"
+            class="round_pagination_button "
+        >
+        </button>
+        <p>{{ currentSlide + 1 }}/{{ props.quotes.length }}</p>
+      </div>
+
     </div>
   </div>
 
   <div
       v-else
-      class="inspirationContainer cursor-pointer relative w-full min-h-[28.4rem] rounded-[40px] p-[1.2rem] text-left mb-[2rem] max-small:mb-0 flex items-center justify-center"
+      class="inspirationContainer cursor-pointer w-full h-[35rem] rounded-[40px] p-[1.2rem] text-left mb-[2rem] max-small:mb-0 "
       :style="{'background-color': `#ddd7c8`}"
   >
-
-    <div class="text-center text-[#000] iconDiv">
+    <div class="quoteIcon ">
+      <img :src="Quotation" class="w-[2.5rem] h-[2.5rem] justify-end " alt="quote icone"/>
+    </div>
+    <!--    Front start-->
+    <div class="text-center text-[#000] iconDiv h-full flex flex-col items-center justify-center">
       <img :src="props.icon" class="w-[11.3rem] z-50 h-[11.7rem] mx-auto" alt="about speaker bright uk"/>
       <header class="text-[2.2rem] leading-[24px] font-[700] mt-[1.5rem]">{{ props.title }}</header>
     </div>
-    <div
-        class="qoutation text-[#000] font-[400] text-[1.4rem] px-[1.6rem] leading-[20px] absolute top-0 left-0 pt-[4rem] pb-[0.5rem]">
-      <img :src="Quotation" class="absolute top-[1.5rem] right-[1.5rem] w-[2.5rem] z-50 h-[2.5rem]"
-           alt="about speaker bright uk"/>
-      <div class="flex flex-row gap-x-[0.5rem] mb-[1rem] items-center">
-        <img :src="props.icon" class="w-[2.5rem] z-50 h-[2.5rem]" alt="about speaker bright uk"/>
-        <header class="text-[1.8rem] leading-[24px] font-[700]">{{ props.title }}</header>
-      </div>
-      <div class="flex flex-col justify-between">
+    <!--    Front end-->
+    <!--    Back start-->
+    <div class="qoutation">
+      <div>
+        <div class="flex flex-row gap-x-[0.5rem] mb-[1rem] items-center">
+          <img :src="props.icon" class="w-[2.5rem] z-50 h-[2.5rem]" alt="about speaker bright uk"/>
+          <header class="text-[1.8rem] leading-[24px] font-[700]">{{ props.title }}</header>
+        </div>
         <div class="flex mb-2">
-          <div  v-for="(slide, index) in props.quotes" :key="index" v-show="index === currentSlide">
-            <p class="text-left">{{ slide }}</p>
+          <div v-for="(slide, index) in props.quotes" :key="index" v-show="index === currentSlide">
+            <p class="text-left">{{ slide.substring(0, 350) }}</p>
           </div>
         </div>
-        <div class="flex space-x-[0.5rem] justify-end items-center">
-          <button
-              v-for="(_, index) in props.quotes"
-              :key="`${index}`+'button'"
-              @click="slideTo(index)"
-              :class="index === currentSlide ? 'bg-black text-white' : 'bg-gray-50 text-black'"
-              class="round_pagination_button "
-          >
-          </button>
-          <p>{{currentSlide + 1}}/{{props.quotes.length}}</p>
-        </div>
       </div>
+
+      <div class="flex space-x-[0.5rem] justify-end items-center mt-auto">
+        <button
+            v-for="(_, index) in props.quotes"
+            :key="`${index}`+'button'"
+            @click="slideTo(index)"
+            :class="index === currentSlide ? 'bg-black text-white' : 'bg-gray-50 text-black'"
+            class="round_pagination_button "
+        >
+        </button>
+        <p>{{ currentSlide + 1 }}/{{ props.quotes.length }}</p>
+      </div>
+
     </div>
+    <!--    Back End-->
   </div>
 </template>
 
@@ -111,7 +121,14 @@ const quotesConfig = {
   display: none;
   width: 0;
   transition: all 1s ease-in-out;
+  @apply text-[#000] font-[400] text-[1.4rem] px-[1.6rem] leading-[20px] pb-[0.5rem];
 
+}
+
+.quoteIcon{
+  display: none;
+  width: 0;
+  transition: all 1s ease-in-out;
 }
 
 .inspirationContainer:hover {
@@ -120,8 +137,17 @@ const quotesConfig = {
 
 .inspirationContainer:hover .qoutation {
   width: 100%;
-  display: block;
+  display: flex;
+  flex-direction: column;
+  justify-items: left;
+  padding-bottom: 35px;
   transition: all 1s ease-in-out;
+  height: 100%;
+}
+
+.inspirationContainer:hover .quoteIcon {
+  transition: all 1s ease-in-out;
+  @apply flex justify-end w-full;
 }
 
 .inspirationContainer:hover .iconDiv {
@@ -130,6 +156,6 @@ const quotesConfig = {
 }
 
 .round_pagination_button {
-  @apply  p-[7px] rounded-full flex items-center justify-center border-[0.1rem] border-[#F1F1F1] font-bold;
+  @apply p-[7px] rounded-full flex items-center justify-center border-[0.1rem] border-[#F1F1F1] font-bold;
 }
 </style>
